69b3c2aaedd04365f55357e5821985e59ec9319a
BetsyBiegon <betsybiegon@gmail.com>
2024-04-05 10:25:28 +0300

    add changes

--- test/helpers.js
+++ test/helpers.js
@@ -1,20 +1,24 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const chai = require('chai');
+global.expect = chai.expect;
+const fs = require('fs');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
 const babel = require('babel-core');
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+// Read HTML file
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
 
+// Transpile JavaScript code
 const babelResult = babel.transformFileSync(
   path.resolve(__dirname, '..', 'index.js'), {
     presets: ['env']
   }
 );
 
-const src = babelResult.code
+const src = babelResult.code;
 
+// Set up DOM environment
 jsdom({
-  html, src
+  html,
+  src
 });
